01542f00293016ac2f4c788f4c78744d4aeb67e6
eve <46179055+eve86@users.noreply.github.com>
2020-04-28 15:51:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,11 @@ $(function () {
   $(".hide-nav").click(function () {
     $(this).fadeOut();
   });
+  var loader = $('.loader-wrap');
+  $(window).on('load', function () {
+    loader.fadeOut();
+  });
+  setTimeout(function () {
+    loader.fadeOut();
+  }, 3000);
 });
-
